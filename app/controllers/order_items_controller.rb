9bc34110499e93f916c86f6616f6fe102e974773
Umut Akturk <umut.akturk14@gmail.com>
2023-10-14 04:02:26 +0300

    Menu and dropdown done.

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -1,7 +1,17 @@
 class OrderItemsController < ApplicationController
   def destroy
     order_item = OrderItem.find(params[:id])
+    order = order_item.order
     authorize order_item
-    order_item.destroy
+    if order_item.quantity > 1
+      order_item.quantity -= 1
+      order.total_price -= order_item.item.price.to_d
+      order.save!
+      order_item.save!
+    else
+      order.total_price -= order_item.item.price.to_d
+      order.save!
+      order_item.destroy
+    end
   end
 end
--- db/migrate/20231008222011_create_orders.rb
+++ db/migrate/20231008222011_create_orders.rb
@@ -3,7 +3,7 @@ class CreateOrders < ActiveRecord::Migration[7.0]
     create_table :orders do |t|
       t.references :user, null: false, foreign_key: true
       t.datetime :order_date
-      t.decimal :total_price
+      t.decimal :total_price, default: 0.0, null: false
 
       t.timestamps
     end
